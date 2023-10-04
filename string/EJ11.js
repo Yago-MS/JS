f3a70c1682eb4cbaf52d157ab32a97c0f7139517
Yago-MS <dmorsil0912@g.educaand.es>
2023-10-04 17:33:49 +0200

    EJ11 hecho

---
+++ string/EJ11.js
@@ -0,0 +1,21 @@
+function uncamelize(text, separator = " "){
+    let split = text.split("");
+    let aux, aux2;
+    for (let i = 0; i < text.length; i++) {
+
+        if(split[i]===split[i].toUpperCase()){
+            split.push("");
+            aux = split[i].toLowerCase();
+            split[i] = separator;
+            for (let j = i+1; j < split.length; j++) {
+                aux2 = aux;
+                aux = split[j];
+                split[j] = aux2;
+            }
+
+        }
+    }
+    return split.join("")
+}
+
+console.log(uncamelize("helloWorld"))
