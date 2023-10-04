5925227abec0d39fa8a1b3fcff10a5fe9538adad
Yago-MS <dmorsil0912@g.educaand.es>
2023-10-04 16:19:57 +0200

    EJ4 hecho

--- string/EJ4.js
+++ string/EJ4.js
@@ -1,1 +1,9 @@
-Hola
+function truncate_string(name, limit){
+    let cut = "";
+    for (let i = 0; i < limit; i++) {
+        cut += name.charAt(i);
+    }
+    return cut;
+}
+
+console.log(truncate_string("Yago Morales Silva", 3))
