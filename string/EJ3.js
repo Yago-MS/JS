e4930e0b57338f03b7469593c160c1309701cb2a
Yago-MS <dmorsil0912@gmail.com>
2023-10-05 08:46:36 +0200

    EJ3 arreglado

--- string/EJ3.js
+++ string/EJ3.js
@@ -1,8 +1,12 @@
-let str = "zZhOla";
+let str = "Hola";
 let chars = [];
 for (let i = 0; i < str.length; i++) {
 
+    if(str.charCodeAt(i)!==122){
     chars.push(String.fromCharCode((str.charCodeAt(i) === 90? 65: str.charCodeAt(i)+1)));
+    }else{
+    chars.push(String.fromCharCode((97)))
+    }
 }
 str = chars.join('');
 console.log(str);
