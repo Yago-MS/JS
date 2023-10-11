8f43564bac0ed7df04c8d273764ee933969fb5fa
Yago-MS <dmorsil0912@gmail.com>
2023-10-11 08:31:26 +0200

    EJ3 arreglado

--- string/EJ5.js
+++ string/EJ5.js
@@ -4,4 +4,14 @@ function abbrev_name(name_surname){
     return name[0] + " " + name[1]+ ".";
 }
 
+function abbrev_name2(name_surname){
+    let name = name_surname.split(" ");
+    nameAbrev = name[0] +" ";
+
+    for (let i = 1; i < name.length; i++) {
+        nameAbrev += name[i].charAt(0) + ". ";
+    }
+    return nameAbrev;
+}
 console.log(abbrev_name("Yago Morales"));
+console.log(abbrev_name2("Yago Morales Silva"))
