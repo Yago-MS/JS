9e1c10d90308a6091d25ba01a87f33b84a6c4bfa
Yago-MS <dmorsil0912@g.educaand.es>
2024-01-10 17:36:15 +0100

    date en proceso

--- contact_form/EJ1/script.js
+++ contact_form/EJ1/script.js
@@ -1,27 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     let name = document.querySelector("#name");
     let surname = document.querySelector("#surname");
-    let birthday = document.querySelector("#birthday");
+    let dp = new AirDatepicker("#birthday");
     let gender = document.querySelector("#gender");
     let email = document.querySelector("#email");
     let phone = document.querySelector("#phone");
     let button = document.querySelector("#submit")
     console.log(button);
-    let contactBook = { contacts:[
-            {name:"testName", surname:"testLastname",birthday:"testBirthday",gender:"testGender",email:"testEmail",phone:"testPhoneNumber"}
-        ]};
-    button.addEventListener("click", function (){
-        let everythingOk = true;
-        everythingOk = !validator.isEmpty("#name");
-        everythingOk = !validator.isEmpty("#surname");
-        everythingOk = validator.isDate("#birthday");
-        everythingOk = !validator.isEmpty("#gender");
-        everythingOk = validator.isEmail("#email");
-        everythingOk = validator.isMobilePhone("#phone");
-        if(everythingOk){
-            console.log("DE LOCOOOOOS")
-        }else {
-            console.log("nop")
+    let contactBook = {
+        contacts: [
+            {}
+        ]
+    };
+    button.addEventListener("click", function () {
+        if(!validator.isEmpty(dp.$el.value)){
+            let date = dp.$el.value.split(".");
+            console.log(dp.formatDate(new Date(2024, 10, 31), "yyyy/mm/dd"));
+            console.log(dp.formatDate(new Date(parseInt(date[2]), parseInt(date[1]) -1 , parseInt(date[0])), "yyyy/mm/dd"))
+            if (!validator.isEmpty(name.value) &&
+                !validator.isEmpty(surname.value) &&
+                validator.isDate(dp.formatDate(new Date(), "yyyy/mm/dd")) &&
+                !validator.isEmpty(gender.value) &&
+                validator.isEmail(email.value) &&
+                validator.isMobilePhone(phone.value)) {
+                console.log("todo cul")
+            }
         }
+
+
     })
 })
