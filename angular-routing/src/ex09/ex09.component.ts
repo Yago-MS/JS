313a3111d632b38debe2f515964bca3b45e623ce
Yago-MS <dmosil0912@g.educaand.es>
2024-02-16 14:41:17 +0100

    haciendo angular

--- angular-routing/src/ex09/ex09.component.ts
+++ angular-routing/src/ex09/ex09.component.ts
@@ -16,6 +16,14 @@ export class Ex09Component {
 
   constructor() { }
 
+  sortEmployees(type:string){
+    if(type=="name")
+      this.employees.sort( (a, b) => {return a.name> b.name ? 1 : a.name == b.name ? 0 : -1});
+    if(type="salary")
+      this.employees.sort((a, b) => {return a.salary -b.salary});
+    else
+      this.employees.sort( (a, b) => {return a.position> b.position ? 1 : a.position == b.position ? 0 : -1});
+  }
   ngOnInit(): void {
     this.employees = EMPLOYEE_LIST;
   }
